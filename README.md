26054588c081a6332833061076fee89880c4c30e
Rotvic <victormfdezfdez@gmail.com>
2023-11-28 15:32:43 +0100

    Funcionalidad aplicada
    Funcionalidad implementada, no muy óptima pero funcional.

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
 # calculadoraJS
-Esta es mi primera calculadora creada en JS, un proyecto simple pero perfecto para no quedarme quieto en mi preparación.
+Esta es mi primera calculadora creada en JS, un proyecto simple pero perfecto para expandir un poco mis conocimientos.
+Con este proyecto he estudiado:
+ - El funcionamiento del algoritmo Shunting Yard y el como aplicarlo.
+ - Reforzado las estructuras básicas de JS.
 
+Además de entretenerme bastante haciéndolo!! 😆
+
+## Funcionalidades
+
+- Suma, resta, multiplicación y división.
+- Operaciones con números decimales.
+- Interfaz fácil de usar con disposición similar a calculadoras científicas.
+- Función de borrado para corregir la entrada.
+
+## Uso
+
+1. Descarga o clona el repositorio en tu máquina local.
+2. Abre el archivo `index.html` en tu navegador web.
+3. Utiliza los botones de la calculadora para introducir y calcular expresiones matemáticas.
+
+## Personalización
+
+Si deseas personalizar el estilo o la funcionalidad de la calculadora, puedes modificar el código HTML, CSS y JavaScript según tus necesidades.
+
+### Estilo (CSS)
+
+El estilo de la calculadora se controla principalmente en el archivo `style.css`. Puedes ajustar los colores, tamaños y diseños según tus preferencias.
+
+### Lógica (JavaScript)
+
+La lógica de la calculadora se encuentra en el archivo `script.js`. Si deseas realizar cambios en cómo se manejan las operaciones o agregar nuevas características, puedes hacerlo aquí.
+
+## Contribuciones
+
+¡Las contribuciones son bienvenidas! Soy consciente que el código no es óptimo actualmente, pero quería tener un primer versionado del proyecto 😁
--- script.js
+++ script.js
@@ -6,7 +6,6 @@ $(document).ready(function() {
 
     números.each(function () {
         $(this).on("click", function(){
-            
             resultado = $(".resultado").val()
             resultado = resultado + $(this).text();
             pintar(resultado)
@@ -15,7 +14,6 @@ $(document).ready(function() {
 -
     operator.each(function () {
         $(this).on("click", function(){
-            
             resultado = $(".resultado").val()
             resultado = resultado + $(this).text();
             pintar(resultado)
@@ -34,11 +32,10 @@ $(document).ready(function() {
 })
 
 function calcular() {
-    try {
-        // 1+1 -> (string)(string)(string)
-    } catch (error) {
-        
-    }
+    let calculo = $(".resultado").val()
+
+    $(".resultado").val(calcularExpresion(calculo))
+
 }
 
 function limpiar(){
@@ -47,4 +44,107 @@ function limpiar(){
 
 function pintar(valor) {
     $(".resultado").val(valor)
+}
+
+function mostrarMensaje( mensaje) {
+   alert(mensaje)
+}
+
+// Realización del parser manualmente
+
+function calcularExpresion(expresion) {
+    // Tokenizar la expresión
+    const tokens = tokenize(expresion);
+
+    // console.log(tokens)
+    // Convertir la expresión en una estructura de árbol
+    const arbol = construirArbol(tokens);
+    console.log(arbol)
+    // Evaluar la expresión a través del árbol
+    const resultado = evaluarArbol(arbol);
+    // console.log(resultado)
+    return resultado;
+}
+
+function tokenize(expresion) {
+    // Implementa la lógica de tokenización usando expresión regular
+    // Divide la expresión en números, operadores y devuelve un array de tokens
+    //Como observación, no contempla los números negativos
+    return expresion.match(/[^\d()]+|[\d.]+/g);
+
+}
+
+function construirArbol(tokens) {
+    // Implementar la lógica para convertir la expresión en una estructura de árbol
+    // Se usa el algoritmo Shunting Yard para esto, pero lo vamos a aplicar en escala de los 4 operadores que ya tenemos
+    // ['5', '+', '10', '*', '2', '-', '4']  => ['5','10','x','+','4','-']
+    let operators = [];
+    let resultado = [];
+    tokens.forEach(element => {
+        if(esNumero(element)){
+            resultado.push(element)
+        }else{
+            if(element == "*" || element == "/" && operators.slice(-1) == "+" || operators.slice(-1) == "-"){
+                operators.unshift(element);
+            }else if(element == "*" || element == "/" && operators.slice(0) == "*" || operators.slice(0) == "/"){
+                resultado.push(operators.shift())
+                operators.unshift(element);
+            }else if(element == "+" || element == "-" && operators.slice(-1) == "+" || operators.slice(-1) == "-"){
+                resultado = resultado.concat(operators);
+                operators = []
+                operators.push(element)
+            }else if(element == "+" || element == "-" && operators.slice(-1) == "*" || operators.slice(-1) == "/"){
+                resultado = resultado.concat(operators);
+                operators = []
+                operators.push(element)
+            }
+        }
+    });
+
+    return resultado = resultado.concat(operators);
+
+}
+
+//Comprueba si es un número
+function esNumero(token) {
+    return !isNaN(parseFloat(token)) && isFinite(token);
+}
+
+//Comprueba si es un operador
+function esOperador(token) {
+    return token === '+' || token === '-' || token === '*' || token === '/';
+}
+
+function evaluarArbol(arbol) {
+    // Implementar la lógica para evaluar la expresión desde la estructura de árbol
+    // Recorrer el árbol y realizar las operaciones matemáticas
+    let pila = [];
+
+    for (let i = 0; i < arbol.length; i++) {
+        let token = arbol[i];
+
+        if (esNumero(token)) {
+            pila.push(parseFloat(token));
+        } else if (esOperador(token)) {
+            let operand2 = pila.pop();
+            let operand1 = pila.pop();
+
+            switch (token) {
+                case '+':
+                    pila.push(operand1 + operand2);
+                    break;
+                case '-':
+                    pila.push(operand1 - operand2);
+                    break;
+                case '*':
+                    pila.push(operand1 * operand2);
+                    break;
+                case '/':
+                    pila.push(operand1 / operand2);
+                    break;
+            }
+        }
+    }
+
+    return pila.pop();
 }
